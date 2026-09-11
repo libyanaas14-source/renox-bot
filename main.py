ee127bb53c5cd481b082cfd6ba2f27b1e90158a2
libyanaas14-source <libyanaas14@gmail.com>
2026-09-11 02:01:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -63,18 +63,20 @@ class DiscordBot(commands.Bot):
     def __init__(self) -> None:
         intents = discord.Intents.default()
         intents.message_content = True
-
         super().__init__(
             command_prefix=commands.when_mentioned,
             intents=intents,
         )
-async def setup_hook(self) -> None:
+
+    async def setup_hook(self) -> None:
         synced_commands = await self.tree.sync()
         logging.info("Synced %d command(s) globally", len(synced_commands))
 
     async def on_ready(self) -> None:
         if self.user is not None:
             logging.info("Logged in as %s (ID: %s)", self.user, self.user.id)
+
+
 bot = DiscordBot()
 
 
